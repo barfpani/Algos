d3caaa88b0654e0208a0094aaa8fc39ca1694c3a
barpani <sanchit7368@gmail.com>
2024-11-11 23:04:55 +0530

    added two new algorithms

---
+++ Searching/binary_search.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+// Iniatialization of function and variables which are required in this function
+
+int binary_search(int arr[], int size, int target){
+    int low = 0;
+    int high = size - 1;
+
+        while(low <= high){
+            int mid = (low + high)/ 2;      // Spliting up array in half
+        
+            if (arr[mid] == target){                                                    // If the target element is at mid then ending 
+                printf("Target: %d was found at this postion: %d\n", target, mid);      // the loop and returning pos of taget in that array.
+                return mid;                                                              
+            }                                                                           
+            else if (arr[mid] < target){    // Searching the element in right half
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;             // Searching the element in left half
+            }
+        } 
+    return -1; 
+}
+int main(){
+
+    // Initialzation of variables
+
+    int arr[] = {2, 5, 7, 9, 12};
+    int target = 9;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    int result = binary_search(arr, size, target);  //calling out the function
+
+    if(result == -1){
+        printf("Targeted element: %d is not in this array\n ", target);
+    }
+    return 0;
+}
---
+++ Sorting/bubble_sort.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (A[j] > A[j + 1]) {
+                swap(&A[j], &A[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int A[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    bubble_sort(A, n);
+
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
