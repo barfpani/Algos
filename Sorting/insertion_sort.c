00d67f3380f0198d40dadb5e6df43f1a3665cd19
barpani <sanchit7368@gmail.com>
2024-11-13 23:22:07 +0530

    Added new sorting algorithm

---
+++ Sorting/insertion_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void insertion_sort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key; 
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    insertion_sort(arr, size);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
