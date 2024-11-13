dfecdb83b0d9664998cd1c1bde9c9bee9a3b0af8
barpani <sanchit7368@gmail.com>
2024-11-14 00:15:59 +0530

    updation and added some comments

--- Sorting/insertion_sort.c
+++ Sorting/insertion_sort.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+    //Initializtion of the function
+
 void insertion_sort(int arr[], int size) {
     for (int i = 1; i < size; i++) {
         int key = arr[i];
         int j = i - 1;
 
+        // shifts neighboring elements of the key which are greater than the key.
+
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
+
+        //placing the key in its correct position after the comparison
+
         arr[j + 1] = key; 
     }
 }
@@ -16,9 +23,13 @@ void insertion_sort(int arr[], int size) {
 int main() {
     int arr[] = {5, 2, 9, 1, 5, 6};
     int size = sizeof(arr) / sizeof(arr[0]);
+    
+    //Calling our function
 
     insertion_sort(arr, size);
 
+    //Printing our sorting array
+    
     printf("Sorted array: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
