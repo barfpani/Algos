92edb54a50002bfdc2707d3e9322fe3ef7a0363d
barpani <sanchit7368@gmail.com>
2024-11-17 22:50:00 +0530

    added necessary comments

--- Sorting/quick_sort.c
+++ Sorting/quick_sort.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
-//Initialization of functions which are going to be used in this algorithm
+//INITIALIZATION OF THE FUNCTIONS WHICH ARE GOING TO BE USED IN THIS ALGORITHM.
+
+
+//Swap function...which will swap the elements based on the pivot
 
 void swap(int* a,  int* b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
+
+
+//Partition function, which will divide the array into two parts, and those two in two more.
 
 int partition(int arr[], int low, int high){
     int pivot = arr[high]; 
@@ -22,6 +28,9 @@ int partition(int arr[], int low, int high){
     return i + 1;
 }
 
+
+//Quicksort function which will handle all the sorting.
+
 void quick_sort(int arr[], int low, int high){
     if(low < high) {
         int pi = partition(arr, low, high);
@@ -33,11 +42,18 @@ void quick_sort(int arr[], int low, int high){
 
 int main(){
     
+    //Initializing array and other variables
+
     int arr[] = {5, 9, 7, 3, 7, 6, 1, 8};
     int n = sizeof(arr)/ sizeof(arr[0]);
 
+    
+    //Calling the function
+    
     quick_sort(arr, 0, n - 1);
 
+    //Printing the sorted array 
+    
     printf("Sorted arrary: ");
     for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
