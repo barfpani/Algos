dd5728818b59bbe5585538d75d5a1bad9f1d50c1
barpani <sanchit7368@gmail.com>
2024-11-25 22:50:12 +0530

    Updation in bubble sort

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+//Initializing functions which are going to be used in our algorithm.
+
+//This function will take care of swapping elements.
+
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
+
+//Main algorithm function, bubble sort takes compares two elements from the starting
+//of the list, and swap them if the first one is bigger tha the second one.
 
 void bubble_sort(int A[], int n) {
     for (int i = 0; i < n - 1; i++) {
@@ -17,12 +24,27 @@ void bubble_sort(int A[], int n) {
 }
 
 int main() {
+
+    //Initializing our list of numbers which we want to sort.
+
     int A[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(A) / sizeof(A[0]);
 
+    //Printing out unsorted array.
+
+    printf("Orignal Arrar: ");
+    for(int k = 0; k < n; k++){
+        printf("%d ", A[k]);
+    }
+    printf("\n");
+
+    // Calling our sorting function and passing our list and it's size.
+
     bubble_sort(A, n);
 
-    printf("Sorted array: \n");
+    //Printing the sorted array.
+
+    printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", A[i]);
     }
