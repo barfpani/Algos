d4d0c56449fde8a1e485980598151139222c2195
barpani <sanchit7368@gmail.com>
2025-03-18 14:32:13 +0530

    linked_revised

--- DS/linked.c
+++ DS/linked.c
@@ -1,101 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a linked list node
-typedef struct Node {
+typedef struct Node{
     int data;
     struct Node* next;
-} Node;
+}Node;
 
-// Function to create a new node
-Node* createNode(int data) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    if (!newNode) {
-        printf("Memory allocation failed\n");
-        return NULL;
+Node* createnode(int data){
+    Node* newNode = malloc(sizeof(Node));
+        if(!newNode){
+            printf("Memomry allocation failed");
+            return 0;
+        }
+
+        newNode -> data = data;
+        newNode -> next = NULL;   
+        return newNode; 
+}
+
+int insert_at_front(Node** head, int data){
+    Node* newNode = createnode(data);
+    if(!newNode){
+        return 0;
     }
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
-
-// Function to insert a node at the beginning of the list
-void insertAtBeginning(Node** head, int data) {
-    Node* newNode = createNode(data);
-    if (!newNode) return;
-    newNode->next = *head;
+    newNode -> next = *head;
     *head = newNode;
+    return 0;
 }
 
-// Function to insert a node at the end of the list
-void insertAtEnd(Node** head, int data) {
-    Node* newNode = createNode(data);
-    if (!newNode) return;
-    if (*head == NULL) {
+int insert_at_back(Node** head,int data){
+    Node* newNode = createnode(data);
+    if(!newNode)return 0;
+    if(*head == NULL){
         *head = newNode;
-        return;
+        return 0;
     }
     Node* temp = *head;
-    while (temp->next != NULL) {
-        temp = temp->next;
+    while(temp -> next != NULL){
+        temp = temp -> next;
     }
-    temp->next = newNode;
+    temp -> next = newNode;
+    return 0;
 }
 
-// Function to delete a node by value
-void deleteByValue(Node** head, int value) {
-    if (*head == NULL) {
+void delete_by_value(Node** head, int value){
+    if(*head == NULL){
         printf("List is empty\n");
         return;
     }
+    
     Node* temp = *head;
-
-    // If the node to be deleted is the head
-    if (temp->data == value) {
-        *head = temp->next;
+    if(temp -> data == value){
+        *head = temp -> next;
         free(temp);
         return;
     }
+    
+    Node *prev = NULL;
 
-    Node* prev = NULL;
-    while (temp != NULL && temp->data != value) {
+    while(temp != NULL && temp -> data != value){
         prev = temp;
-        temp = temp->next;
+        temp = temp -> next;
     }
-    if (temp == NULL) {
-        printf("Value not found in the list\n");
+    if(temp == NULL){
+        printf("Element not found\n");
         return;
     }
-    prev->next = temp->next;
+    prev -> next = temp -> next;
     free(temp);
+    return;
 }
 
-// Function to traverse the list and print its elements
-void traverseList(Node* head) {
-    Node* temp = head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
+void traverse_list(Node** head){
+    Node* temp = *head;
+    while(temp != NULL){
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
     }
     printf("NULL\n");
 }
 
-// Main function
-int main() {
+int main(){
+    
     Node* head = NULL;
 
-    // Insert elements
-    insertAtBeginning(&head, 10);
-    insertAtBeginning(&head, 20);
-    insertAtEnd(&head, 30);
+    insert_at_front(&head, 50);
+    insert_at_front(&head, 20);
+    insert_at_front(&head, 40);
+    insert_at_back(&head, 30);
 
-    printf("Linked List after insertion: ");
-    traverseList(head);
+    traverse_list(&head);
 
-    // Delete an element
-    deleteByValue(&head, 20);
-    printf("Linked List after deletion: ");
-    traverseList(head);
+    delete_by_value(&head, 20);
 
+    traverse_list(&head);
+    
     return 0;
 }
