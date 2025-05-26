c00a610934bc1472ef4c18c8caf7703ac3b97ea6
Sanchit <164382838+barfpani@users.noreply.github.com>
2025-05-26 23:27:35 +0530

    Update binary_search.c

--- Searching/binary_search.c
+++ Searching/binary_search.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-// Iniatialization of function and variables which are required in this function
+// Iniatialization of function and variables which are required in this algoithm.
 
 int binary_search(int arr[], int size, int target){
     int low = 0;
     int high = size - 1;
 
         while(low <= high){
-            int mid = (low + high)/ 2;      // Spliting up array in half
+            int mid = (low + high)/ 2;                                      // Spliting up array in half
         
-            if (arr[mid] == target){                                                    // If the target element is at mid then ending 
-                printf("Target: %d was found at this postion: %d\n", target, mid);      // the loop and returning pos of taget in that array.
+            if (arr[mid] == target){                                        // If the target element is at mid then ending 
+                printf("Target: %d was found at: %d\n", target, mid);       // the loop and returning pos of taget in that array.
                 return mid;                                                              
             }                                                                           
-            else if (arr[mid] < target){    // Searching the element in right half
+            else if (arr[mid] < target){                                    // Searching the element in right half
                 low = mid + 1;
             }
             else{
-                high = mid - 1;             // Searching the element in left half
+                high = mid - 1;                                             // Searching the element in left half
             }
         } 
     return -1; 
@@ -30,7 +30,7 @@ int main(){
     int target = 9;
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int result = binary_search(arr, size, target);  //calling out the function
+    int result = binary_search(arr, size, target);                           //calling out the function
 
     if(result == -1){
         printf("Targeted element: %d is not in this array\n ", target);
