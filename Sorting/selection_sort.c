b36bbbc4be2ea756cc321d4f5392156e187846dd
barpani <sanchit7368@gmail.com>
2025-06-10 16:31:33 +0530

    Selection_sort Complete

--- Sorting/selection_sort.c
+++ Sorting/selection_sort.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 
 void selection_sort(int arr[], int size){
+    //loop for comparing the first element.
     for(int i = 0; i < size; i++){
         int min_ele = i;
-
+        
+        // Loop for comparing the first element with the rest of the elements;
         for(int j = i + 1; j < size; j++){
-            if(arr[j] < arr[min_ele]){
-                min_ele = j;
-            }
+            if(arr[j] < arr[min_ele]){          // The Logic is very simple, Assume that the first element is the smallest
+                min_ele = j;                    // and compare every other elements with it, if any element is smaller than
+            }                                   // that element? Swap Them.
         }
+        // Basic "Swap Function"
         int temp = arr[i];
         arr[i] = arr[min_ele];
         arr[min_ele] = temp;
