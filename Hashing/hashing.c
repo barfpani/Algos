6197b1102f2c85531f3b2541f30f4a342bd4a76e
barpani <sanchit7368@gmail.com>
2025-06-14 23:59:33 +0530

    Hashing with new hash fxn completed

--- Hashing/hashing.c
+++ Hashing/hashing.c
@@ -23,3 +23,74 @@ unsigned int hash(char *str){
     return hash % TABLE_SIZE;
 }
 
+void insert (char *key, int value){
+    unsigned int index = hash(key);
+
+    Hashnode *newnode = malloc(sizeof(Hashnode));
+    newnode -> key = strdup(key);
+    newnode -> value = value;
+    newnode -> next = hashtable[index];
+
+    //Now moving this newnode at the front of that index if there are already elements present.
+    hashtable[index] = newnode;
+}
+
+int search (char *key, int *outvalue){
+    unsigned int index = hash(key);
+    Hashnode *node = hashtable[index];
+
+    while (node != NULL){
+        if(strcmp(node -> key, key) == 0){
+            outvalue = node -> value;
+            return 1;
+        }
+        node = node -> next;
+    }
+    return 0;
+}
+
+void free_table(){
+    for (int i = 0; i < TABLE_SIZE; i++){
+        Hashnode *node = hashtable[i];
+        while(node){
+            Hashnode *temp = node;
+            node = node -> next;
+            free(temp -> key);
+            free(temp);
+        }
+        hashtable[i] = NULL;
+    }
+}
+
+void print_table(){
+    for(int i = 1; i < TABLE_SIZE; i++){
+        printf("[%d]: ", i);
+        Hashnode *node = hashtable[i];
+        while(node){
+            printf("(%s: %d) -> ", node -> key, node -> value);
+            node = node -> next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main(){
+    insert("Sanchit", 20);
+    insert("Rachit", 19);
+    insert("Saad", 22);
+    insert("Adya", 21);
+    insert("Divyam", 20);
+
+    print_table();
+
+    int value = 0;
+    if(search("Adya", &value)){
+        printf("Found Adya: %d\n", value);
+    }
+    else{
+        printf("Adya not found\n");
+    }
+
+    free_table();
+    return 0;
+}
