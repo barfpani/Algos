b9360c7086c3ba64682f356b1658d2f71a0668d4
Sanchit <164382838+barfpani@users.noreply.github.com>
2025-06-17 15:11:32 +0530

    Hashing Documentation

--- Hashing/hashing.c
+++ Hashing/hashing.c
@@ -1,39 +1,42 @@
+// Hash Table implementiation for string-interger pair
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define TABLE_SIZE 10
+#define TABLE_SIZE 10                                        // Defining the hash table size.
 
 typedef struct Hashnode{
-    char* key;
-    int value;
-    struct Hashnode *next;
-
-}Hashnode;
+    char* key;                                              // We are using "chaining" method to handle collision,
+    int value;                                              // the struct contains "key" which'll hold the string,
+    struct Hashnode *next;                                  // "value" which holds the int value, and a next pointer                                                            
+}Hashnode;                                                  // which points to a similar "Hashnode" struct.
 
 Hashnode *hashtable[TABLE_SIZE];
 
-unsigned int hash(char *str){
-    unsigned long hash = 5381;
-    int c;
 
-    while ((c = *str++)){
-        hash = hash * 33 + c;
-    }
-    return hash % TABLE_SIZE;
-}
 
-void insert (char *key, int value){
-    unsigned int index = hash(key);
+unsigned int hash(char* str){                               //  | This is our "Hash Function", and we have used the most popular and efficient hash function
+    unsigned long hash = 5381;                              //  | for stings, which is the "djb2". The type of our "hash" fxn is unsigned bcz we know that it
+    int c;                                                  //  | won't return any negative values. The value of Hash is fixed from the djb2. inside our while loop,
+    while ((c = *str++)){                                   //<-| we are storing each letter's ASCII value from the given string, hashing it and storing it into our
+        hash = hash * 33 + c;                               //  | hash variable, and this loops continues until it encounter /0, wiz end of string, after reaching 
+    }                                                       //  | the end of the string, it returns the index at which the string should be placed, by performing this,
+    return hash % TABLE_SIZE;                               //  | hash % TABLE_SIZE, which will give value between 0 - 9.
+}           
 
-    Hashnode *newnode = malloc(sizeof(Hashnode));
-    newnode -> key = strdup(key);
-    newnode -> value = value;
-    newnode -> next = hashtable[index];
 
-    //Now moving this newnode at the front of that index if there are already elements present.
-    hashtable[index] = newnode;
-}
+
+void insert (char *key, int value){                         //  | This function will insert elements into our "Hashtable", the chaining method will use the concept of 
+    unsigned int index = hash(key);                         //  | linked lists, so the heap memory will be used. It takes 2 parameters, a string and a value for it, we 
+    Hashnode *newnode = malloc(sizeof(Hashnode));           //  | calculate it's index using our "hash" function, The logic is simple, allocate a memory for a newnode   
+    newnode -> key = strdup(key);                           //<-| place it on the correct index and fill it's varirables. Line 30 duplicates the given string to the
+    newnode -> value = value;                               //  | newnode's "key" variable. we could've used "strcpy" but then we would've to manually allocate memory
+    newnode -> next = hashtable[index];                     //  | for it. But in "strdup", it uses alocated heap memory. Line 32 & 33 are crucial. We set the newNode's 
+    hashtable[index] = newnode;                             //  | "next" to point at the head node of that index, (it can be NULL as well) and then set the newnode 
+}                                                           //  | as the new head of the index. (Same logic and operation of addind elements a front in a linked list).
+
+
 
 int search (char *key, int *outvalue){
     unsigned int index = hash(key);
@@ -49,6 +52,8 @@ int search (char *key, int *outvalue){
     return 0;
 }
 
+
+
 void free_table(){
     for (int i = 0; i < TABLE_SIZE; i++){
         Hashnode *node = hashtable[i];
@@ -61,6 +66,8 @@ void free_table(){
         hashtable[i] = NULL;
     }
 }
+
+
 
 void print_table(){
     for(int i = 0; i < TABLE_SIZE; i++){
