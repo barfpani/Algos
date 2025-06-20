5790424860ef47138e5f8b3f3234af354a384c8c
Sanchit <164382838+barfpani@users.noreply.github.com>
2025-06-20 22:38:21 +0530

    Added heapify method

--- Sorting/heap_sort.c
+++ Sorting/heap_sort.c
@@ -25,39 +25,3 @@ void heapify(int arr[], int n, int i) {
     }
 }
 
-// Heap Sort
-void heapSort(int arr[], int n) {
-    // Build a max-heap
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    // Extract elements one by one
-    for (int i = n - 1; i >= 0; i--) {
-        // Move current root to end
-        int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-
-        // Call heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-}
-
-// Print array
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; ++i)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-// Main function
-int main() {
-    int arr[] = {4, 10, 3, 5, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    heapSort(arr, n);
-
-    printf("Sorted array is:\n");
-    printArray(arr, n);
-    return 0;
-}
