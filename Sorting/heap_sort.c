5a1e855bd248f58b61df1b6f1469cca3e65fb556
Sanchit <164382838+barfpani@users.noreply.github.com>
2025-06-23 00:49:47 +0530

    Heapsort Documentation Completed

--- Sorting/heap_sort.c
+++ Sorting/heap_sort.c
@@ -6,42 +6,38 @@ n(log n, for all worst, best and average case.)*/
 
 
 void heapify(int arr[], int n, int i) {                 // This is our main heapify function which will turn any array in a max heap, 
-    int largest = i;                                    // or a min heap. We're generation a max heap.  
-    int left = 2 * i + 1;                               // 
-    int right = 2 * i + 2;                              // 
+    int largest = i;                                    // or a min heap. We're generation a max heap. In a max heap tree, the root has   
+    int left = 2 * i + 1;                               // the highest element and it's childs are equal or smaller. We've supposed that 
+    int right = 2 * i + 2;                              // the largest element is at the first [i], and its child's will be at[2*i+1]L
 
-    // If left child is larger than root
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
+//                                                      // and [2*i+2]R, then we check if the largest element is at the root or not,
+    if (left < n && arr[left] > arr[largest])           // if not? it swaps the largest element with the root. Heapify begins from the last
+        largest = left;                                 // leaf node, in this case [1]. The concept of recursive function is used here
 
-    // If right child is larger than current largest
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
+    if (right < n && arr[right] > arr[largest])         // Working - if the left child is larger than the root? swap them. And if the right
+        largest = right;                                // child is larger than the root? Swap it with root aswell.
 
-    // If largest is not root
-    if (largest != i) {
-        int temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
+    if (largest != i) {                                 // This "if" statement is crucial here, how? this is our breakout statement for our
+        int temp = arr[i];                              // recursion. if the current root is not the largest? then it will swap with the 
+        arr[i] = arr[largest];                          // current largest (from its childs) and call heapify again. If the root is the 
+        arr[largest] = temp;                            // largest then it won't go into the if statement and exit the function.
 
-        // Recursively heapify the affected subtree
-        heapify(arr, n, largest);
+        heapify(arr, n, largest);                       
     }
 }
 
 void heap_sort(int arr[], int n){
 
-    for(int i = n / 2 - 1; i >= 0; i--){
-        heapify(arr, n, i);
-    }
-    for(int i = n - 1; i >= 0; i--){
-        int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-
-        heapify(arr, i, 0);
-    }
-}
+    for(int i = n / 2 - 1; i >= 0; i--){                // The for loops starts at the non leaf node of the tree, and it decrementing so that
+        heapify(arr, n, i);                             // max heaps are formed from the bottom up of the tree. The first for loop will create 
+    }                                                   // a max heap of our array. 
+    for(int i = n - 1; i >= 0; i--){                    // 
+        int temp = arr[0];                              // Inside the second for loop, we'll replace the first (largest) element with the last 
+        arr[0] = arr[i];                                // element, to move it into it's sorted position and then again call the heapify on the
+        arr[i] = temp;                                  // reduced heap. And the for loop is running on the bases of size of the heap? Then it will      
+        heapify(arr, i, 0);                             // terminate automatically and retunrn the sorted array.
+    }                                                   
+}                                                        
 
 void print_array(int arr[], int n){
     for(int i = 0; i < n; i++){
