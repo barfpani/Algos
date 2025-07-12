a90f0d48976a734f1de8ee4f7469551e40c68da0
Sanchit <164382838+barfpani@users.noreply.github.com>
2025-07-13 00:15:36 +0530

    Bloom filter nicely done

--- DS/bloom_filter.c
+++ DS/bloom_filter.c
@@ -5,8 +5,58 @@
 
 #define BLOOM_SIZE 1024
 
-uint8_t Bloom_filter[BLOOM_SIZE / 8]; 
+uint8_t Bloom[BLOOM_SIZE / 8]; 
 
 #define SET_BIT(arr, index) (arr[index / 8] |= (1 << (index % 8)))
 #define GET_BIT(arr, index) (arr[index / 8]  & (1 << (index % 8)))
+
+unsigned int djb2(const char *str){
+    unsigned int hash = 5381;
+    int c;
+    while((c = *str++)){
+        hash = hash * 33 + c;
+    }
+    return hash % BLOOM_SIZE;
+}
+
+unsigned int sdbm(const char *str){
+    unsigned int hash = 0;
+    int c;
+    while((c = *str++)){
+        hash = hash * 65599 + c;
+    }
+    return hash % BLOOM_SIZE;
+}
+
+void bloom_add(const char *str){
+    unsigned int h1 = djb2(str);
+    unsigned int h2 = sdbm(str);
+    SET_BIT(Bloom, h1);
+    SET_BIT(Bloom, h2);
+}
+
+int bloom_check(const char *str){
+    unsigned int h1 = djb2(str);
+    unsigned int h2 = sdbm(str);
+    return GET_BIT(Bloom, h1) && GET_BIT(Bloom, h2);
+}
+
+int main(){
+
+    bloom_add("Apple");
+    bloom_add("banana");
+
+    printf("Check = Apple =");
+    if(bloom_check("Apple\n")){
+        printf("possibly in the set\n");
+    }
+    else{
+        printf("definitely not in the set\n");
+    }
+
+    printf("Check = banana = %s\n", bloom_check("banana")? "Possibly in the set\n" : "Definitley not int the set\n");
+    
+    return 0;
+
+}
 
